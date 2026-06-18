1c1f7a1e9a4c7005e80ecab930b0f16e2616022d
Sharayu Bhosale <sharayubhosale26@gmail.com>
2026-06-18 14:39:43 +0530

    Completed environment configuration

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly configService: ConfigService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getConfig() {
+    return {
+      appName: this.configService.get<string>('APP_NAME'),
+      port: this.configService.get<number>('PORT'),
+    };
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GymModule } from './gym/gym.module';
 
 @Module({
-  imports: [GymModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env.development',
+
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+        APP_NAME: Joi.string().required(),
+      }),
+    }),
+
+    GymModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
